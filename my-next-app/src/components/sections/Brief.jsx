906af162673cba1325bffade06017556aab03a89
TLucas <lucastai0601@gmail.com>
2025-06-26 11:03:54 +0100

    adjusting style

--- my-next-app/src/components/sections/Brief.jsx
+++ my-next-app/src/components/sections/Brief.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 
 
-const ProductList = ()=>{
+const Brief = ()=>{
 
     return(
         <div className="Intro">
-           
+           <h2>Here's the intro of our company</h2>
+           <h4>Placeholder 1</h4>
+           <h4>Placeholder 2</h4>
+           <h4>Placeholder 3</h4>
+           <h4>Placeholder 4</h4>
         </div>
     )
 }
 
-export default ProductList;
+export default Brief;
--- my-next-app/src/components/sections/MultiWindowDisplay.jsx
+++ my-next-app/src/components/sections/MultiWindowDisplay.jsx
@@ -2,10 +2,27 @@
 import React from 'react';
 import {useContext, useEffect,useState} from "react";
 import placeholder from '../../assets/images/placeholder.jpg';
+import { usePathname } from 'next/navigation';
 
 
 const MultiWindowDisplay = () => {
   const [image, setImage] = useState('');
+  const pathname = usePathname();
+
+  const getBackground = (numbering) => {
+    switch (numbering) {
+      case 0:
+        return '/images/home.jpg';
+      case 1:
+        return '/images/about.jpg';
+      case 2:
+        return '/images/about.jpg';
+      case 3:
+        return '/images/about.jpg';
+      default:
+        return '/images/default.jpg';
+    }
+  };
 
   useEffect(() =>{
     setImage(placeholder);
@@ -19,7 +36,7 @@ const MultiWindowDisplay = () => {
           
           <div className="col-md-6">
             <div className="box box-1">
-              → learn more
+              → Learn more
             </div>
           </div>
           <div className="  col-md-6"> 
@@ -27,20 +44,20 @@ const MultiWindowDisplay = () => {
             <div className="grid_display_mid row gx-0 gy-0">
               <div className="col-sm-6">
                 <div className=" box box-2">
-                  → learn more
+                  → Learn more
                 </div>
               </div>
               <div className="col-sm-6">
                 <div className=" box box-3">
-                  → learn more
+                  → Learn more
                 </div>
               </div>
             </div>
             
-            <div className="grid_display_mid row">
+            <div className="grid_display_mid row ">
               <div className="col-sm-12">
-                <div className="box box-4">
-                    → learn more
+                <div className="box box-4 ">
+                    → Learn more
                 </div>
                 </div>
             </div>
