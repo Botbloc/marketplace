eaa94e22cc18580cb3eb202bb2dc59c8645989ad
Lucas Tai <lucastai0601@gmail.com>
2025-07-31 01:17:43 +0800

    fixing shopping cart styling

--- my-next-app/src/app/cart/page.jsx
+++ my-next-app/src/app/cart/page.jsx
@@ -8,21 +8,59 @@ import Link from "next/link";
 const template_product = [
     {
         "id" : 0,
-        "product_name" : "product 1",
+        "product_name" : "Product 1",
         "price" : 100,
-        "currency" : "$"
+        "currency" : "$",
+        "quantity" : 3
     },
     {
         "id" : 1,
-        "product_name" : "product 2",
+        "product_name" : "Product 2",
         "price" : 300,
-        "currency" : "$"
+        "currency" : "$",
+        "quantity" : 2
     },
     {
         "id" : 2,
-        "product_name" : "product 3",
+        "product_name" : "Product 3",
         "price" : 600,
-        "currency" : "$"
+        "currency" : "$",
+        "quantity" : 4
+    },
+    {
+        "id" : 3,
+        "product_name" : "Product 4",
+        "price" : 600,
+        "currency" : "$",
+        "quantity" : 4
+    },
+    {
+        "id" : 4,
+        "product_name" : "Product 5",
+        "price" : 600,
+        "currency" : "$",
+        "quantity" : 4
+    },
+    {
+        "id" : 5,
+        "product_name" : "Product 6",
+        "price" : 6000,
+        "currency" : "$",
+        "quantity" : 1
+    },
+    {
+        "id" : 6,
+        "product_name" : "Product 7",
+        "price" : 60,
+        "currency" : "$",
+        "quantity" : 1
+    },
+    {
+        "id" : 7,
+        "product_name" : "Product 8",
+        "price" : 200,
+        "currency" : "$",
+        "quantity" : 8
     }
 ]
 
@@ -52,8 +90,14 @@ const cart = () =>{
                     return(
                         <Link href={href+item.id}>
                             <div className="product_in_cart" key={item.id}>
-                                <h4>{item.product_name}</h4>
-                                <h5>{item.currency + " " + item.price}</h5>
+                                <div className="item_name_module">
+                                    <h4>{item.product_name}</h4>
+                                </div>
+                                <div className="item_price_module">
+                                    <div className="cell">{item.currency + " " + item.price}</div>
+                                    <div className="cell">{item.quantity}</div>
+                                    <div className="cell">{item.currency + " " + item.price * item.quantity}</div>
+                                </div>
                             </div>
                         </Link>
                         
@@ -97,26 +141,46 @@ const cart = () =>{
             </div>
         )
     }
-
+    const dummy = 120;
     return(
         product_in_cart != undefined
         && <div className="cart_window">
-            <div>
-                <h4>Shopping Cart</h4>
-            </div>
-            <div className="row">
-                <div className="col-md-6 left_pane">
-                    
-                    {generateProductEntity(product_in_cart)}
-                </div>
-                <div className="col-md-6 right_pane">
-                    <div>
-                        {displayCalculation()}
-                    </div>
+            <div className="cart_heading">
+
+                    <h4>Your Shopping Cart</h4>
+                
+                <div className="heading_purchase">
+                    <h4>Total: ${dummy.toFixed(2)}</h4>
                     <button>Proceed to checkout</button>
                 </div>
-
+                
             </div>
+            
+            <div className="left_pane">
+                <div className="product_header">
+                    <div>
+                        <div className="cell">
+                            product
+                        </div>
+                    </div>
+                        
+                    <div className="item_price_module_header">
+                        <div className="cell">
+                            Unit Price
+                        </div>
+                        <div className="cell">
+                            Qty
+                        </div>
+                        <div className="cell">
+                            Subtotal
+                        </div>
+                    </div>
+                </div>
+                {generateProductEntity(product_in_cart)}
+            </div>
+                
+
+            
         </div>        
     )
 } 
