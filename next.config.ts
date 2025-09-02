79a94522c07ab4533098198a10d30eac5ac448f7
Lucas Tai <lucastai0601@gmail.com>
2025-09-02 19:37:18 +0800

    minor fix

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
   output: 'export',
   images: { unoptimized: true },
   basePath: '/marketplace',
-  assetPrefix: '/marketplace',
+  assetPrefix: '/marketplace/',
 };
 
 export default nextConfig;
